5116f4d5e08fadf161d3dd27c5b1612f1247416f
Clement <clecocel@gmail.com>
2018-02-03 15:47:54 -0800

    Initial files

---
+++ data_generator.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python2.7
+
+IMAGES_PATH = './SCUT-FBP5500/Images'
+RATINGS_PATH = './SCUT-FBP5500/ratings_mean.csv'
+
---
+++ resnet.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2.7
+
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+from keras.layers import Dense
+import numpy as np
+
+base_model = ResNet50(weights='imagenet', include_top=False)
+
+x = base_model.output
+
+# We flatten then output 1 value (regression - maybe we should use classification)
+x = Flatten()(x)
+prediction = Dense(1)(x)
+
+# this is the model we will train
+model = Model(inputs=base_model.input, outputs=prediction)
+
+# first: train only the top layers (which were randomly initialized)
+# i.e. freeze all convolutional InceptionV3 layers
+for layer in base_model.layers:
+	layer.trainable = False
+
+# compile the model (should be done *after* setting layers to non-trainable)
+model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae', 'mse'])

69fb78792d0d5d93fcceb7a56e8354f3c832c5a8
clecocel <clecocel@users.noreply.github.com>
2018-02-04 11:21:07 -0800

    new training schedule

--- face_rating/processing/resnet.py
+++ face_rating/processing/resnet.py
@@ -9,6 +9,9 @@ import numpy as np
 
 base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(224,224,3), pooling='avg')
 
+
+
+#############################################################
 x = base_model.output
 # let's add a fully-connected layer
 x = Dense(1024, activation='relu', kernel_regularizer=regularizers.l2(0.01),
@@ -21,24 +24,28 @@ prediction = Dense(1, kernel_regularizer=regularizers.l2(0.02),
                 activity_regularizer=regularizers.l1(0.02))(x)
 # this is the model we will train
 model1 = Model(inputs=base_model.input, outputs=prediction)
+#############################################################
 
 
-
+#############################################################
 x = base_model.output
 prediction = Dense(1, kernel_regularizer=regularizers.l2(0.02),
                 activity_regularizer=regularizers.l1(0.02))(x)
 model2 = Model(inputs=base_model.input, outputs=prediction)
+#############################################################
 
 
-
+#############################################################
 x = base_model.output
 #x = Flatten()(x)
 prediction = Dense(1, kernel_regularizer=regularizers.l2(0.02),
                 activity_regularizer=regularizers.l1(0.02))(x)
 # this is the model we will train
 model3 = Model(inputs=base_model.input, outputs=prediction)
+#############################################################
 
 
+#############################################################
 x = base_model.output
 #x = Flatten()(x)
 x = Dense(128, activation='relu', kernel_regularizer=regularizers.l2(0.01),
@@ -47,7 +54,7 @@ prediction = Dense(1, kernel_regularizer=regularizers.l2(0.02),
                 activity_regularizer=regularizers.l1(0.02))(x)
 # this is the model we will train
 model4= Model(inputs=base_model.input, outputs=prediction)
-
+#############################################################
 
 
 
@@ -59,6 +66,8 @@ for layer in base_model.layers:
 
 base_model2 = ResNet50(weights='imagenet', include_top=False, input_shape=(224,224,3))
 
+
+#############################################################
 x = base_model2.output
 x = Flatten()(x)
 # let's add a fully-connected layer
@@ -72,25 +81,29 @@ prediction = Dense(1, kernel_regularizer=regularizers.l2(0.02),
                 activity_regularizer=regularizers.l1(0.02))(x)
 # this is the model we will train
 model5 = Model(inputs=base_model2.input, outputs=prediction)
+#############################################################
 
 
-
+#############################################################
 x = base_model2.output
 x = Flatten()(x)
 prediction = Dense(1, kernel_regularizer=regularizers.l2(0.02),
                 activity_regularizer=regularizers.l1(0.02))(x)
 model6 = Model(inputs=base_model2.input, outputs=prediction)
+#############################################################
 
 
-
+#############################################################
 x = base_model2.output
 x = Flatten()(x)
 prediction = Dense(1, kernel_regularizer=regularizers.l2(0.02),
                 activity_regularizer=regularizers.l1(0.02))(x)
 # this is the model we will train
 model7 = Model(inputs=base_model2.input, outputs=prediction)
+#############################################################
 
 
+#############################################################
 x = base_model2.output
 x = Flatten()(x)
 x = Dense(128, activation='relu', kernel_regularizer=regularizers.l2(0.01),
@@ -99,7 +112,7 @@ prediction = Dense(1, kernel_regularizer=regularizers.l2(0.02),
                 activity_regularizer=regularizers.l1(0.02))(x)
 # this is the model we will train
 model8= Model(inputs=base_model2.input, outputs=prediction)
-
+#############################################################
 
 # first: train only the top layers (which were randomly initialized)
 # i.e. freeze all convolutional InceptionV3 layers
--- face_rating/processing/train.py
+++ face_rating/processing/train.py
@@ -8,7 +8,10 @@ BATCH_SIZE = 128
 
 training_generator, training_samples, test_set, test_samples = main(batch_size=BATCH_SIZE)
 
-
+def write_results(filename, history):
+    with open(filename, 'w') as f:
+        print(history.history['mean_absolute_error'], file=f)
+        print(history.history['val_mean_absolute_error'], file=f)
 
 def train(model):
     model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae', 'mse'])
@@ -16,7 +19,7 @@ def train(model):
     history = model.fit_generator(
         training_generator,
         steps_per_epoch=training_samples // BATCH_SIZE,
-        epochs=50,
+        epochs=20,
         callbacks=None,
         validation_data=test_set)
     return history
@@ -24,13 +27,22 @@ def train(model):
 results = []
 
 results.append(train(model1))
+write_results('results_model1.txt', results[-1])
 results.append(train(model2))
+write_results('results_model2.txt', results[-1])
 results.append(train(model3))
+write_results('results_model3.txt', results[-1])
 results.append(train(model4))
+write_results('results_model4.txt', results[-1])
 results.append(train(model5))
+write_results('results_model5.txt', results[-1])
 results.append(train(model6))
+write_results('results_model6.txt', results[-1])
 results.append(train(model7))
+write_results('results_model7.txt', results[-1])
 results.append(train(model8))
+write_results('results_model8.txt', results[-1])
+
 
 with open('training_results.txt', 'w') as f:
     for result in results:

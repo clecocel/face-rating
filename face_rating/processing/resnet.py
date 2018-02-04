0c43f77b25a8dd3c48178e3560909b07758a750a
clecocel <clecocel@users.noreply.github.com>
2018-02-03 18:54:11 -0800

    change file loc

--- face_rating/processing/resnet.py
+++ face_rating/processing/resnet.py
@@ -1,18 +1,19 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2.7
 
 from keras.applications.resnet50 import ResNet50
 from keras.preprocessing import image
 from keras.applications.resnet50 import preprocess_input, decode_predictions
-from keras.layers import Dense, Flatten
-from keras.models import Model
+from keras.layers import Dense
 import numpy as np
 
 base_model = ResNet50(weights='imagenet', include_top=False)
 
 x = base_model.output
 
-# We flatten then output 1 value (regression - maybe we should use classification)
-x = Flatten()(x)
+
+x = GlobalAveragePooling2D()(x)
+# let's add a fully-connected layer
+x = Dense(1024, activation='relu')(x)
 prediction = Dense(1)(x)
 
 # this is the model we will train
@@ -21,7 +22,7 @@ model = Model(inputs=base_model.input, outputs=prediction)
 # first: train only the top layers (which were randomly initialized)
 # i.e. freeze all convolutional InceptionV3 layers
 for layer in base_model.layers:
-    layer.trainable = False
+	layer.trainable = False
 
 # compile the model (should be done *after* setting layers to non-trainable)
 model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae', 'mse'])

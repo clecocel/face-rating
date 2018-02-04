e633cb2153996818fbdc8ccfbb7e43e44ab25adf
clecocel <clecocel@users.noreply.github.com>
2018-02-04 11:00:29 -0800

    multi training

--- face_rating/processing/train.py
+++ face_rating/processing/train.py
@@ -1,24 +1,43 @@
 #!/usr/bin/env python3
 
 from data_generator import main
-from resnet import model
+from resnet import *
 from keras.optimizers import SGD
 
-BATCH_SIZE = 64
+BATCH_SIZE = 128
 
 training_generator, training_samples, test_set, test_samples = main(batch_size=BATCH_SIZE)
 
-model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae', 'mse'])
 
-history = model.fit_generator(
-    training_generator,
-    steps_per_epoch=training_samples // BATCH_SIZE,
-    epochs=1,
-    callbacks=None,
-    validation_data=test_set)
 
-print(history.history['mean_absolute_error'])
-print(history.history['val_mean_absolute_error'])
+def train(model):
+    model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae', 'mse'])
+
+    history = model.fit_generator(
+        training_generator,
+        steps_per_epoch=training_samples // BATCH_SIZE,
+        epochs=50,
+        callbacks=None,
+        validation_data=test_set)
+    return history
+
+results = []
+
+results.append(train(model1))
+results.append(train(model2))
+results.append(train(model3))
+results.append(train(model4))
+results.append(train(model5))
+results.append(train(model6))
+results.append(train(model7))
+results.append(train(model8))
+
+with open('training_results.txt', 'w') as f:
+    for result in results:
+        print(result.history['mean_absolute_error'], file=f)
+        print(result.history['val_mean_absolute_error'], file=f)
+        print('------------------', file=f)
+
 '''
 model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='mean_squared_error', metrics=['mae', 'mse'])
 

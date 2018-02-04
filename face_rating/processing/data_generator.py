ad1dd4a58b794e13f2c9d4c109016b5caaa3e48a
Robin Schucker <rschucker@greencharge.net>
2018-02-03 17:47:34 -0800

    created generator function to load images and scores

--- face_rating/processing/data_generator.py
+++ face_rating/processing/data_generator.py
@@ -1,5 +1,60 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 
-IMAGES_PATH = './SCUT-FBP5500/Images'
-RATINGS_PATH = './SCUT-FBP5500/ratings_mean.csv'
+import os
+import numpy as np
+import pandas as pd
 
+from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
+from sklearn.model_selection import train_test_split
+
+FACE_RATING_HOME = '/home/rschucker/Documents/face'  # os.environ.get('FACE_RATING_HOME')
+
+DATA_DIR = 'data'
+IMAGES_PATH = os.path.join(FACE_RATING_HOME, DATA_DIR, 'Images')
+RATINGS_PATH = os.path.join(FACE_RATING_HOME, DATA_DIR, 'ratings_mean.csv')
+IMAGE_COL = 'Image'
+RATING_COL = 'Rating'
+
+RANDOM_STATE = 0
+
+
+def load_image(path, target_size=(224, 224)):
+    img = load_img(path, target_size=target_size, interpolation='bicubic')
+    x = img_to_array(img)
+    return x
+
+
+def load_x_y(data_path, rating_path, target_size=(224, 224)):
+    all_jpg_files = [f for f in os.listdir(data_path) if f.endswith('.jpg')]
+    scores_s = load_scores(rating_path)
+    scores_list = [scores_s[f] for f in all_jpg_files]
+    images = [load_image(os.path.join(data_path, f), target_size=target_size) for f in all_jpg_files]
+
+    return np.array(images), np.array(scores_list)
+
+
+def load_scores(rating_path):
+    df = pd.read_csv(rating_path)
+    df.index = df[IMAGE_COL]
+    series = df[RATING_COL]
+    return series
+
+
+def main(target_size=(224, 224), test_split=0.20, batch_size=32):
+    x, y = load_x_y(IMAGES_PATH, RATINGS_PATH, target_size=target_size)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_split, random_state=RANDOM_STATE)
+    datagen = ImageDataGenerator(
+        rotation_range=20,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest')
+
+    gen = datagen.flow(x_train, y_train, batch_size=batch_size)
+    return gen, x_train.shape[0], (x_test, y_test), x_test.shape[0]
+
+
+if __name__ == '__main__':
+    main()
--- face_rating/processing/pre_processing.py
+++ face_rating/processing/pre_processing.py
@@ -1,39 +1,43 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 
 import time
 import scipy.stats
 import numpy as np
 
+
 def get_mode(list_ratings):
-	return scipy.stats.mode(list_ratings)[0][0]
+    return scipy.stats.mode(list_ratings)[0][0]
+
 
 def get_mean(list_ratings):
-	return np.mean(list_ratings)
+    return np.mean(list_ratings)
+
 
 def get_median(list_ratings):
-	return np.median(list_ratings)
+    return np.median(list_ratings)
+
 
 def write_computed_ratings(image_ratings, computation_fn, filename):
-	with open(filename, 'w') as f:
-		f.write('Image,Rating')
-		for image_name in image_ratings:
-			f.write('\n{},{}'.format(image_name, computation_fn(image_ratings[image_name])))
+    with open(filename, 'w') as f:
+        f.write('Image,Rating')
+        for image_name in image_ratings:
+            f.write('\n{},{}'.format(image_name, computation_fn(image_ratings[image_name])))
+
 
 if __name__ == '__main__':
 
-	image_ratings = {}
+    image_ratings = {}
 
-	with open('All_ratings.csv', 'rU') as f:
-		# Drop the column descriptions
-		next(f)
-		for line in f:
-			rater, image_name, rating, orig_rating = line.split(',')
-			if image_name in image_ratings:
-				image_ratings[image_name].append(int(rating))
-			else:
-				image_ratings[image_name] = [int(rating)]
+    with open('All_ratings.csv', 'rU') as f:
+        # Drop the column descriptions
+        next(f)
+        for line in f:
+            rater, image_name, rating, orig_rating = line.split(',')
+            if image_name in image_ratings:
+                image_ratings[image_name].append(int(rating))
+            else:
+                image_ratings[image_name] = [int(rating)]
 
-
-	write_computed_ratings(image_ratings, get_mode, 'ratings_mode.csv')
-	write_computed_ratings(image_ratings, get_mean, 'ratings_mean.csv')
-	write_computed_ratings(image_ratings, get_median, 'ratings_median.csv')
+    write_computed_ratings(image_ratings, get_mode, 'ratings_mode.csv')
+    write_computed_ratings(image_ratings, get_mean, 'ratings_mean.csv')
+    write_computed_ratings(image_ratings, get_median, 'ratings_median.csv')

0912fdf898b24f590f715fc6552476471b3d427d
Robin Schucker <rschucker@greencharge.net>
2018-02-05 19:49:28 -0800

    fixed issue #11 by resizing large images to max(width, height) = 800 + improved test script

--- face_rating/processing/test_face_detector.py
+++ face_rating/processing/test_face_detector.py
@@ -5,24 +5,36 @@
 import cv2
 from face_rating.processing import face_detector
 import matplotlib.pyplot as plt
+import os
 
 
 def plot_image(image):
     plt.figure()
-    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    plt.imshow(image)
     plt.show(block=False)
 
 
-def main():
-    path = '/home/rschucker/Documents/face/data/test_images/860_main_beauty.png'
-    image = cv2.imread(path)
+def main(path='/home/rschucker/Documents/face/data/test_images/860_main_beauty.png'):
+    image = cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB)
+    image = face_detector.reshape_to_max_dimensions(image, max_one_dimension_pixel=800)
+
     plot_image(image)
     all_faces = face_detector.isolate_faces(image)
+    print('found: {} faces'.format(len(all_faces)))
     for face in all_faces:
         plot_image(face)
     plt.show()
     return
 
+
+def test_large_faces(dir_path='/home/rschucker/Documents/face/data/test_images/large_faces'):
+    all_files = [f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))]
+    for f in all_files:
+        print(f)
+        plt.close('all')
+        main(os.path.join(dir_path, f))
+
 if __name__ == '__main__':
-    main()
+    main('/home/rschucker/Documents/face/data/test_images/large_faces/16.jpg')
+    # test_large_faces()
 

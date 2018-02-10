62926c03b52156a27e77311bd6c101621674defe
clecocel <clecocel@users.noreply.github.com>
2018-02-10 12:23:16 -0800

    add stats

--- face_rating/processing/stats.py
+++ face_rating/processing/stats.py
@@ -1,13 +1,15 @@
 from keras.models import load_model
+import tensorflow as tf
 from data_generator import main
 
 def test(filename):
-	training_generator, training_samples, test_set, test_samples = main(
-    	batch_size=BATCH_SIZE, data_augmentation=False, test_split=1)
-	x_test, y_test = test_set
+    training_generator, training_samples, test_set, test_samples = main(
+        batch_size=BATCH_SIZE, data_augmentation=False, test_split=1)
+    x_test, y_test = test_set
+    sess = tf.Session()
     model = load_model(filename + '.h5')
-    preds = model.predict(x_test, batch_size=64)
+    preds = model.predict(x_test, batch_size=1)
     with open('stats.txt', 'w') as f:
-    	print(preds, file=f)
+        print([p[0] for p in preds], file=f)
 
 test('train_last_layers_mse_augmentation_on/results_model10_adam_lr0.001_run1_last20')
